b006d11382bef336c024d56fd6425491311b0c91
wlgud <wlgud3412@naver.com>
2026-06-23 15:42:42 +0900

    timestamp JsonFormat

--- src/main/java/com/jihyoung/plant_disease_detection_web_spring/global/dto/ErrorResponse.java
+++ src/main/java/com/jihyoung/plant_disease_detection_web_spring/global/dto/ErrorResponse.java
@@ -1,11 +1,14 @@
 package com.jihyoung.plant_disease_detection_web_spring.global.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDateTime;
 
 public record ErrorResponse(
         int status,
         String code,
         String message,
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
         LocalDateTime timestamp
 ) {
     public ErrorResponse(
