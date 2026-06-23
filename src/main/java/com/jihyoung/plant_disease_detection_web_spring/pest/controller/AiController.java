7b76fdee7504d086718b7345800451b847be637d
wlgud <wlgud3412@naver.com>
2026-06-23 11:30:47 +0900

    ai controller 생성

---
+++ src/main/java/com/jihyoung/plant_disease_detection_web_spring/pest/controller/AiController.java
@@ -0,0 +1,28 @@
+package com.jihyoung.plant_disease_detection_web_spring.pest.controller;
+
+import com.jihyoung.plant_disease_detection_web_spring.pest.dto.ai.AiPredictResponse;
+import com.jihyoung.plant_disease_detection_web_spring.pest.service.AiService;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+public class AiController {
+
+    private final AiService aiService;
+
+    public AiController (AiService aiService)
+    {
+        this.aiService = aiService;
+    }
+
+    @PostMapping(value = "/predict", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public AiPredictResponse predict(
+            @RequestParam("cropName") String cropName,
+            @RequestParam("image") MultipartFile image
+    ) {
+        return aiService.predict(cropName, image);
+    }
+}
