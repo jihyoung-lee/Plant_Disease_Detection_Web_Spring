ad7483d29a09599e2193ea61ad35c4d836de9978
wlgud <wlgud3412@naver.com>
2026-06-23 11:37:49 +0900

    json property 추가

--- src/main/java/com/jihyoung/plant_disease_detection_web_spring/pest/dto/ai/AiPredictResponse.java
+++ src/main/java/com/jihyoung/plant_disease_detection_web_spring/pest/dto/ai/AiPredictResponse.java
@@ -1,8 +1,15 @@
 package com.jihyoung.plant_disease_detection_web_spring.pest.dto.ai;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record AiPredictResponse (
+
+        @JsonProperty("crop_name")
         String cropName,
+
+        @JsonProperty("sick_name_kor")
         String sickNameKor,
+
         double confidence
 ) {
 
